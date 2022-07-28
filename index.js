0d1228b7624be9e937c70dea65f30113c0401789
Derek Lobo <dereklobo89@gmail.com>
2022-07-28 09:02:50 -0400

    graphql setup and first graphql query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema.js';
 
 const app = express();
 
@@ -6,6 +8,14 @@ app.get('/', (req, res) => {
   res.send('Hello World! Welcome to GraphQL');
 });
 
+const root = { hello: () => 'Hello World! Welcome to root' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
 app.listen(8000, () => {
-  console.log('Example app listening on port localhost:8000!');
+  console.log('Example app listening on port localhost:8000/graphql');
 });
